2c33b0fcddc7e6fc5d560d64218a71f729b62989
Caio B. Dutra <caio.dutra@facedigital.com.br>
2022-05-30 16:49:52 -0300

    Fifth class done. #06

--- cursoVue/2_projeto_vue/src/App.vue
+++ cursoVue/2_projeto_vue/src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <primeiro-componente></primeiro-componente>
+    <div>
+        <primeiro-componente></primeiro-componente>
+        <p>O dado de app é: {{ testando }}</p>
+    </div>
 </template>
 
 <script>
-  import PrimeiroComponente from './components/PrimeiroComponente.vue'
+import PrimeiroComponente from "./components/PrimeiroComponente.vue";
 
-  export default {
-    name: 'App',
+export default {
+    name: "App",
     components: {
-      PrimeiroComponente
-    }
-  }
+        PrimeiroComponente,
+    },
+    data() {
+        return {
+            testando: "Testando",
+        };
+    },
+};
 </script>
