2890293542234f6a9ec21c17eaef87d90ae6890c
Caio B. Dutra <caio.dutra@facedigital.com.br>
2022-05-31 15:51:08 -0300

    Sixth class one. #07

--- cursoVue/2_projeto_vue/src/App.vue
+++ cursoVue/2_projeto_vue/src/App.vue
@@ -2,16 +2,19 @@
     <div>
         <primeiro-componente></primeiro-componente>
         <p>O dado de app é: {{ testando }}</p>
+        <life-cycle></life-cycle>
     </div>
 </template>
 
 <script>
 import PrimeiroComponente from "./components/PrimeiroComponente.vue";
+import LifeCycle from "./components/LifeCycle.vue";
 
 export default {
     name: "App",
     components: {
         PrimeiroComponente,
+        LifeCycle,
     },
     data() {
         return {
